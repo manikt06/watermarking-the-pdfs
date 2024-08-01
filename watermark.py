1af149579e33ce1d9a5d428bf9ef39197435a011
MANIK TYAGI <84763811+manikt06@users.noreply.github.com>
2024-08-01 15:07:20 +0530

    Add files via upload

---
+++ watermark.py
@@ -0,0 +1,17 @@
+import PyPDF2
+
+
+
+template = PyPDF2.PdfReader(open('11.3 twopage.pdf', 'rb'))
+watermark = PyPDF2.PdfReader(open('11.1 wtr.pdf', 'rb'))
+output = PyPDF2.PdfWriter()
+
+
+
+for i in range(len(template.pages)):
+  page = template.pages[i]
+  page.merge_page(watermark.pages[0])
+  output.add_page(page)
+
+  with open('watermarked_output.pdf', 'wb') as file:
+    output.write(file)
